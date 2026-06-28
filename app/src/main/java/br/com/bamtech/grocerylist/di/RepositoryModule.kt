9a55475bff3c18dccc4536ba70d155ce2790fd31
pedro <pedromuab@gmail.com>
2026-06-27 22:01:16 -0300

    SPRINT 3 fix: RepositoryModule to singletonComponent

--- app/src/main/java/br/com/bamtech/grocerylist/di/RepositoryModule.kt
+++ app/src/main/java/br/com/bamtech/grocerylist/di/RepositoryModule.kt
@@ -5,13 +5,15 @@ import br.com.bamtech.grocerylist.domain.repository.GroceryRepository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
-@InstallIn(ViewModelComponent::class)
+@InstallIn(SingletonComponent::class)
 abstract class RepositoryModule {
 
     @Binds
+    @Singleton
     abstract fun bindRepository(
         impl: GroceryRepositoryImpl
     ): GroceryRepository
