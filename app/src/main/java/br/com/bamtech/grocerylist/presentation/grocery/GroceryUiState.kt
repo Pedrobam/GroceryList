fa53a855b171f63bd663a18bc52382ed1a963a9c
pedro <pedromuab@gmail.com>
2026-07-21 21:08:07 -0300

    SPRINT 4 refactor: - GroceryUiState.kt

--- app/src/main/java/br/com/bamtech/grocerylist/presentation/grocery/GroceryUiState.kt
+++ app/src/main/java/br/com/bamtech/grocerylist/presentation/grocery/GroceryUiState.kt
@@ -2,8 +2,8 @@ package br.com.bamtech.grocerylist.presentation.grocery
 
 import br.com.bamtech.grocerylist.domain.model.GroceryItem
 
-sealed class GroceryUiState {
-    data object Loading: GroceryUiState()
-    data class Success(val items: List<GroceryItem>): GroceryUiState()
-    data class Error(val message: String): GroceryUiState()
+sealed interface GroceryUiState {
+    data object Loading: GroceryUiState
+    data class Success(val items: List<GroceryItem>): GroceryUiState
+    data class Error(val message: String): GroceryUiState
 }
